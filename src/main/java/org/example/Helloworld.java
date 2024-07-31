0177ea374c50c01bb500ecf3eed4573ba5e5c282
neeraj <neerajtestingdata@gmail.com>
2024-07-31 14:01:36 -0400

    Initail

---
+++ src/main/java/org/example/Helloworld.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Helloworld
+{
+    public static void main(String aa[])
+
+    {
+        System.out.println("Hello world");
+
+    }
+public static void printsomething()
+{
+    System.out.println("Print something");
+
+}}
